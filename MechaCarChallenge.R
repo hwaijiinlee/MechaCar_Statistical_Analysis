c10b14d9080c32d9d50c767f7c0a91913afed55b
Hwai Jiin Lee <hwaijiinlee@gmail.com>
2022-01-02 11:51:01 -0600

    updating Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,23 @@
 library(dplyr)
+#Deliverable 1
 MechaCar_df <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MechaCar_df) #perform multiple linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MechaCar_df))
+
+#Deliverable 2
 suspension_table <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in dataset
 total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+
+#Deliverable 3
+t.test(suspension_table$PSI,mu=1500) #t-test across all Lots against population
+
+#subset each lot separately
+lot1 <- subset(suspension_table, Manufacturing_Lot=="Lot1")
+lot2 <- subset(suspension_table, Manufacturing_Lot=="Lot2")
+lot3 <- subset(suspension_table, Manufacturing_Lot=="Lot3")
+
+#t-test each lot against population
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
