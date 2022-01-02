62b5b16c58625c38ca4a05d84e0b7de473ff47bf
Hwai Jiin Lee <hwaijiinlee@gmail.com>
2022-01-01 21:44:20 -0600

    updating deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,6 @@ library(dplyr)
 MechaCar_df <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) #read in dataset
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MechaCar_df) #perform multiple linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = MechaCar_df))
+suspension_table <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in dataset
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
